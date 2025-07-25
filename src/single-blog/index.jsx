f67832b20b215a7f9736cb184cf7e47e25d2e800
Andrew1Ashraf <andrewashrafadeb565@gmail.com>
2025-07-25 04:01:36 +0300

    first commit

--- src/single-blog/index.jsx
+++ src/single-blog/index.jsx
@@ -3,28 +3,8 @@ import "../styles/single-blog.css";
 function SingleBlog() {
   return (
     <section className="single-blog-page">
-      {/* <nav>
-        <img src="/assets/single-blog/foodieland..svg" alt="logo" />
-
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Recipes</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#">About</a></li>
-        </ul>
-
-        <div className="icons">
-          <i className="fa-brands fa-facebook" style={{ fontSize: "22px" }}></i>
-          <i className="fa-brands fa-x-twitter" style={{ fontSize: "22px" }}></i>
-          <i className="fa-brands fa-instagram" style={{ fontSize: "22px" }}></i>
-        </div>
-      </nav>
-
-      <hr /> */}
-
       <section>
-        <div className="container">
+        <div className="container-single-blog">
           <h1 className="First-section-title">
             Full Guide to Becoming a Professional Chef
           </h1>
@@ -34,10 +14,8 @@ function SingleBlog() {
               <img src="/assets/single-blog/Ellipse 2.svg" alt="pic" />
               <span className="author-name">John Smith</span>
             </div>
-            <hr />
-            <div className="date">
-              <span className="date">15 March 2022</span>
-            </div>
+            <div className="Dash"></div>
+            <span className="date">15 March 2022</span>
           </div>
 
           <p style={{ width: "80%", textAlign: "center", opacity: "60%" }}>
@@ -59,28 +37,37 @@ function SingleBlog() {
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Curabitur ac ultrices odio. Nulla at congue diam, at dignissim
-                turpis. Ut vehicula sed velit a faucibus. In feugiat
-                vestibulum velit vel pulvinar. Fusce id mollis ex. Praesent
-                feugiat elementum ex ut suscipit.
+                turpis. Ut vehicula sed velit a faucibus. In feugiat vestibulum
+                velit vel pulvinar. Fusce id mollis ex. Praesent feugiat
+                elementum ex ut suscipit.
               </p>
             </div>
 
             <div className="share">
               <span>SHARE ON:</span>
-              <i className="fa-brands fa-facebook" style={{ fontSize: "22px" }}></i>
-              <i className="fa-brands fa-x-twitter" style={{ fontSize: "22px" }}></i>
-              <i className="fa-brands fa-instagram" style={{ fontSize: "22px" }}></i>
+              <i
+                className="fa-brands fa-facebook"
+                style={{ fontSize: "22px" }}
+              ></i>
+              <i
+                className="fa-brands fa-x-twitter"
+                style={{ fontSize: "22px" }}
+              ></i>
+              <i
+                className="fa-brands fa-instagram"
+                style={{ fontSize: "22px" }}
+              ></i>
             </div>
           </div>
 
           <div>
             <h2>What do you like most about your job?</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Curabitur ac ultrices odio. Nulla at congue diam, at dignissim
-              turpis. Ut vehicula sed velit a faucibus. In feugiat vestibulum
-              velit vel pulvinar. Fusce id mollis ex. Praesent feugiat
-              elementum ex ut suscipit.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              ac ultrices odio. Nulla at congue diam, at dignissim turpis. Ut
+              vehicula sed velit a faucibus. In feugiat vestibulum velit vel
+              pulvinar. Fusce id mollis ex. Praesent feugiat elementum ex ut
+              suscipit.
             </p>
           </div>
         </div>
@@ -92,11 +79,11 @@ function SingleBlog() {
             <h2>Do you cook at home on your days off?</h2>
             <img src="/assets/single-blog/Rectangle 23.svg" alt="girl-chef" />
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Curabitur ac ultrices odio. Nulla at congue diam, at dignissim
-              turpis. Ut vehicula sed velit a faucibus. In feugiat vestibulum
-              velit vel pulvinar. Fusce id mollis ex. Praesent feugiat
-              elementum ex ut suscipit.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              ac ultrices odio. Nulla at congue diam, at dignissim turpis. Ut
+              vehicula sed velit a faucibus. In feugiat vestibulum velit vel
+              pulvinar. Fusce id mollis ex. Praesent feugiat elementum ex ut
+              suscipit.
             </p>
 
             <h2>
@@ -104,11 +91,11 @@ function SingleBlog() {
               foot in the door?
             </h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Curabitur ac ultrices odio. Nulla at congue diam, at dignissim
-              turpis. Ut vehicula sed velit a faucibus. In feugiat vestibulum
-              velit vel pulvinar. Fusce id mollis ex. Praesent feugiat
-              elementum ex ut suscipit.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              ac ultrices odio. Nulla at congue diam, at dignissim turpis. Ut
+              vehicula sed velit a faucibus. In feugiat vestibulum velit vel
+              pulvinar. Fusce id mollis ex. Praesent feugiat elementum ex ut
+              suscipit.
             </p>
 
             <h2>
@@ -116,24 +103,24 @@ function SingleBlog() {
               professional chef?
             </h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Curabitur ac ultrices odio. Nulla at congue diam, at dignissim
-              turpis. Ut vehicula sed velit a faucibus. In feugiat vestibulum
-              velit vel pulvinar. Fusce id mollis ex. Praesent feugiat
-              elementum ex ut suscipit.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              ac ultrices odio. Nulla at congue diam, at dignissim turpis. Ut
+              vehicula sed velit a faucibus. In feugiat vestibulum velit vel
+              pulvinar. Fusce id mollis ex. Praesent feugiat elementum ex ut
+              suscipit.
             </p>
           </div>
         </div>
       </section>
 
       <section>
-        <div className="container">
+        <div className="container-single-blog">
           <div className="inbox">
             <h2>Deliciousness to your inbox</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqut enim
-              ad minim
+              eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad
+              minim
             </p>
             <div className="form-group">
               <input type="email" placeholder="your email address" />
@@ -144,13 +131,23 @@ function SingleBlog() {
       </section>
 
       <section className="Recipes">
-        <h2 style={{ fontSize: "36px", textAlign: "center" ,marginBottom:"50px"}}>
+        <h2
+          style={{
+            fontSize: "36px",
+            textAlign: "center",
+            marginBottom: "50px",
+          }}
+        >
           Check out the delicious recipe
         </h2>
         <div className="card-container">
           <div className="card">
             <div className="card-img-wrapper">
-              <img className="badge" src="/assets/single-blog/Group 852.svg" alt="" />
+              <img
+                className="badge"
+                src="/assets/single-blog/Group 852.svg"
+                alt=""
+              />
               <img
                 className="main-img"
                 src="/assets/single-blog/Mask Group (4).svg"
@@ -190,7 +187,11 @@ function SingleBlog() {
 
           <div className="card">
             <div className="card-img-wrapper">
-              <img className="badge" src="/assets/single-blog/Group 852.svg" alt="" />
+              <img
+                className="badge"
+                src="/assets/single-blog/Group 852.svg"
+                alt=""
+              />
               <img
                 className="main-img"
                 src="/assets/single-blog/Mask Group (2).svg"
